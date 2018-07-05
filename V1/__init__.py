2c14024f6ac363c0d318e2e8f16765af588d87b3
Hassan Yahya Hussein <hassan@localhost.localdomain>
2018-07-05 15:35:15 +0300

    [UPDATE] Initialize create_app to run

---
+++ V1/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restplus import Api
+from V1.configurations.config import app_config
+
+api = Api(
+    version='1.0',
+    title='Maintenance Tracker',
+    description='Api with endpoints',
+    prefix='/api/v1')
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(app_config[config_name])
+    api.init_app(app)
+    return app
