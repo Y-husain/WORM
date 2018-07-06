cc944c59b141a61281733af8aa81147e7b7b0547
Hassan Yahya Hussein <hassan@localhost.localdomain>
2018-07-06 20:57:03 +0300

    [update]Table and insert works

--- V1/database/connector.py
+++ V1/database/connector.py
@@ -2,12 +2,42 @@ import pprint as pp
 import psycopg2
 
 
-def DatabaseConnection():
-    """Connection to database"""
-    connection = (
-        "dbname='connect_api' user='hassan' host='localhost' password='yahya' port='5432'"
-    )
-    try:
-        return psycopg2.connect(connection)
-    except:
-        pp.pprint("Cannot connect to database")
+class DatabaseConnection:
+    """Database connection"""
+
+    def __init__(self):
+        try:
+            self.connection = psycopg2.connect(
+                "dbname='connect_api' user='hassan' host='localhost' password='yahya' port='5432'"
+            )
+            self.connection.autocommit = True
+            self.cursor = self.connection.cursor()
+        except:
+            pp.pprint("SORRY cannot connect to database")
+
+    def create_tables(self):
+        try:
+            user_table = """CREATE TABLE users(
+            id SERIAL PRIMARY KEY,
+            firstname VARCHAR(100) NOT NULL,
+            lastname VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            password VARCHAR(100) NOT NULL,
+            secret VARCHAR(100)
+        )"""
+            self.cursor.execute(user_table)
+        except (Exception, psycopg2.DatabaseError) as e:
+            pp.pprint(e)
+
+    def insert_user(self, firstname, lastname, email, password, secret):
+        try:
+            self.cursor.execute(
+                "INSERT INTO users(firstname,lastname, email, password, secret) VALUES(%s,%s,%s,%s,%s)",
+                (firstname, lastname, email, password, secret))
+        except (Exception, psycopg2.IntegrityError) as error:
+            pp.pprint(error)
+
+
+data_connection = DatabaseConnection()
+data_connection.insert_user("yahya", "hussein", "yahya@gmail.com", "12345",
+                            "MyMum")
