4c6ba69b9bee33cdd360f6f79004368c143458bb
Hassan Yahya Hussein <hassan@localhost.localdomain>
2018-07-06 18:44:11 +0300

    [FIX] psycopg2 connect successfully

--- V1/database/connector.py
+++ V1/database/connector.py
@@ -1,25 +1,13 @@
-"""Connection to database with psycopg2"""
-
+import pprint as pp
 import psycopg2
-from V1.configurations.config import app_config
-from psycopg2.extras import RealDictCursor
 
 
-class connectorDB:
-    """Class for database"""
-
-    def init_app(self, config_name):
-        """Initialize connectorDB"""
-        config_object = app_config[config_name]
-        self.db = config_object.DATABASE_URI
-        self.conn = psycopg2.connect(self.db)
-        self.cur = self.conn.cursor(cursor_factory=RealDictCursor)
-
-    def query(self, query):
-        """Query execution cursor"""
-        self.cur.execute(query)
-
-    def close(self):
-        """close connection"""
-        self.cur.close()
-        self.conn.close()
+def DatabaseConnection():
+    """Connection to database"""
+    connection = (
+        "dbname='connect_api' user='hassan' host='localhost' password='yahya' port='5432'"
+    )
+    try:
+        return psycopg2.connect(connection)
+    except:
+        pp.pprint("Cannot connect to database")
